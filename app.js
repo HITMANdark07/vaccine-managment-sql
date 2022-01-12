a00060094436b32571b958d0fdd2ac7fcc028d1d
Shekhar kumar <shekharkumar235838@gmail.com>
2022-01-12 22:53:53 +0530

    corrected port position

--- app.js
+++ app.js
@@ -635,7 +635,7 @@ app.post("/hosp_logindata", authController.isLoggedIn, (req, res) => {
   }
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 /*******************************************************/
 app.listen(PORT, function () {
   console.log('Running on http://localhost:3000');
